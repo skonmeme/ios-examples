79ac7b6a6793705a1b65a2e0a3f23cffad489d14
Sung Gon Yi <skonmeme@gmail.com>
2024-09-05 10:15:33 +0900

    Initial

---
+++ ios-examples/ios_examplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_examplesApp.swift
+//  ios-examples
+//
+//  Created by Sung Gon Yi on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_examplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios-examplesUITests/ios_examplesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ios_examplesUITestsLaunchTests.swift
+//  ios-examplesUITests
+//
+//  Created by Sung Gon Yi on 9/5/24.
+//
+
+import XCTest
+
+final class ios_examplesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
